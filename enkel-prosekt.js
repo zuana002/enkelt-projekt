7888b851ad271e6c6ae522e66d69505cae15f413
zuana002 <zuana002@osloskolen.no>
2026-01-15 10:40:38 +0100

    test
    de er bra og en test

---
+++ enkel-prosekt.js
@@ -0,0 +1,33 @@
+const linje1 = document.getElementById("linje1");
+const linje2 = document.getElementById("linje2");
+const linje3 = document.getElementById("linje3");
+const linje4 = document.getElementById("linje4");
+const linje5 = document.getElementById("linje5");
+const linje6 = document.getElementById("linje6");
+
+
+ 
+function myFunction1() {
+  alert(linje1.innerText);
+  
+  
+}
+ 
+function myFunction2() {
+alert(linje2.innerText);
+}
+ 
+function myFunction3() {
+  alert(linje3.innerText);
+}
+ 
+function myFunction4() {
+  alert(linje4.innerText);
+}
+ 
+function myFunction5() {
+alert(linje5.innerText);
+}
+function myFunction6() {
+alert(linje6.innerText);
+}
